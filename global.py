59b8aa5ab9bb6ce0b56083f20272a9e5c81b78b5
MHBAD <hourmanaf@gmail.com>
2024-08-06 01:55:44 +0300

    Add files via upload

---
+++ global.py
@@ -0,0 +1,17 @@
+x5 ="nice"
+def myfunc5():
+    global x5
+    x5="great5"
+
+print("python is 5" + x5)   
+
+myfunc5()
+
+print("python is 5" + x5) 
+##ex4
+v3=str("2.2")
+print(v3)
+print(type(v3))
+##ex7
+import random
+print(random.randrange(1,5))
